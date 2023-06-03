7489ceba0cc553f97b0ffa7d35b6a38de286166b
Vitto <vitaliy.nykonchuk@gmail.com>
2023-06-03 15:30:43 +0300

    Change app info

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -3,7 +3,7 @@ package _01_initial_commit.app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("My app for Users!");
 
         int a = 8;
         int b = 2;
