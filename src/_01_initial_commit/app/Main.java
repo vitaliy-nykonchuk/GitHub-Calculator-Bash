f4fa8b056e05ee5f86e7ebb0dc2c0b072f0c5d30
Vitto <vitaliy.nykonchuk@gmail.com>
2023-06-03 15:12:04 +0300

    Update app info

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -1,7 +1,17 @@
 package _01_initial_commit.app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("My git app.");
+        System.out.println("My git app. Super!");
+
+        int a = 8;
+        int b = 2;
+        int c = add(a, b);
+        System.out.println(c);
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
     }
 }
