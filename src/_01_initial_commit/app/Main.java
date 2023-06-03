8c68114512dd57d63410d9cecbd9265aa4f2ddfd
Vitto <vitaliy.nykonchuk@gmail.com>
2023-06-03 15:20:30 +0300

    Add multiplication

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -3,14 +3,21 @@ package _01_initial_commit.app;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 80;
-        int b = 20;
+        System.out.println("My git app. Super!");
+
+        int a = 8;
+        int b = 2;
         int c = add(a, b);
         System.out.println(c);
-        System.out.println("Hello, world!");
+        int d = multiply(a, b);
+        System.out.println(d);
     }
 
     private static int add(int a, int b) {
         return a + b;
+    }
+
+    private static int multiply(int a, int b) {
+        return a * b;
     }
 }
